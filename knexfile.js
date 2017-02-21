3b33ab22f4f0a6f808b19cc63ba9c65b20a2f8bb
Siyami Avci <siyamiavci@gmail.com>
2017-02-21 12:57:57 -0800

    Deploy to heroku

--- knexfile.js
+++ knexfile.js
@@ -4,5 +4,10 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/expenses'
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
 };
